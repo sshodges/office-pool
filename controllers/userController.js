77241625dec45048d4394514072c13653326ed02
sshodges <sshodges@gmail.com>
2019-09-07 09:19:02 +1000

    TICKET-6 - Add auth middleware and encryption for JWT token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcrypt');
-const { jwtUtil } = require('../utils-module/index');
-const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const { jwtUtil } = require("../utils-module/index");
+const User = require("../models/User");
 
 exports.register = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
@@ -8,9 +8,9 @@ exports.register = async (req, res) => {
   try {
     // Check if user email exists
     let user = await User.findOne({ email });
-    
+
     if (user) {
-      res.status(400).json({ errorMessage: 'User already exists' });
+      res.status(400).json({ errorMessage: "User already exists" });
     } else {
       // Create User object
       user = new User({
@@ -30,17 +30,12 @@ exports.register = async (req, res) => {
       await user.save();
 
       // Send back token with user id
-      await jwtUtil
-        .createToken(user)
-        .then(token => {
-          res.json({ token });
-        })
-        .catch(error => {
-          res.status(500).json({ error });
-        });
+      await jwtUtil.createToken(user).then(token => {
+        res.json({ token });
+      });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ errorMessage: 'Server Error' });
+    res.status(500).json({ errorMessage: "Server Error" });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { loginValidator } = require('../middleware/request-validation');
-const authController = require('../controllers/authController');
+const { loginValidator } = require("../middleware/request-validation");
+const auth = require("../middleware/auth");
+const authController = require("../controllers/authController");
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', authController.getUser); // Add Auth Middleware when done
+router.get("/", auth, authController.getUser);
 
 // @route   POST api/auth
 // @desc    Login user and create token
 // @access  Public
-router.post('/', loginValidator, authController.login);
+router.post("/", loginValidator, authController.login);
 
 module.exports = router;
