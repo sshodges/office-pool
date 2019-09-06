cd2d28e5b2470b6a441c53d78f3ed0dfc3ba2319
Samuel Hodges <samuel.hodges@4mations-MacBook-Pro.local>
2019-09-06 10:20:44 +1000

    Fix merge issue where express not brought in

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const router = express.Router();
 const { registerValidator } = require('../middleware/request-validation');
 const userController = require('../controllers/userController');
